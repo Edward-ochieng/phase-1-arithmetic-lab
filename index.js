720f88152d6396799049b8aca343bbed22af2160
Edward <edward.ochieng@student.moringaschool.com>
2022-08-08 15:48:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(10*Math.random())
+const num3 = 53;
+const num4 = 7;
+const mod = num3 % num4;
+const max = Math.max(2,4,6,20);
